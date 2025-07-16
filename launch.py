40ab28f29a84e03dc264a0658f04ff6b1cb607fd
mikinol <sm44aksdmiki13877kfh@gmail.com>
2025-07-16 11:03:14 +0300

    Подулучшен launch.py добавлен runbuild чтобы и run и build

--- launch.py
+++ launch.py
@@ -15,10 +15,10 @@ APP_DEV_CMD = ["npm", "run", "dev"]
 LAUNCHER_PATH = os.path.join(BUILD_DIR, "elysium_launcher")
 
 def install_depend():
-    print("Installig npm dependencies syka")
+    print("Installig npm dependencies")
     subprocess.run(["npm", "install"], cwd=APP_DIR, check=True)
-    print("Installig cargo dependencies syka")
-    subprocess.run(["cargo", "install", "cbindgen"], cwd=APP_DIR, check=True)
+    print("Installig cargo dependencies")
+    subprocess.run(["cargo", "install", "cbindgen"], check=True)
 
 def clean_build():
     os.makedirs(BUILD_DIR, exist_ok=True)
@@ -42,10 +42,6 @@ def run_launcher():
     subprocess.run([LAUNCHER_PATH])
 
 def run_dev():
-    os.makedirs(BUILD_DIR, exist_ok=True)
-    shutil.rmtree(BUILD_DIR)
-    os.makedirs(BUILD_DIR)
-
     # Start npm run dev
     npm_proc = subprocess.Popen(APP_DEV_CMD, cwd=APP_DIR)
 
@@ -66,28 +62,40 @@ def run_dev():
     build_cmake()
     run_launcher()
 
+def run_build():
+    build_app()
+    configure_cmake()
+    build_cmake()
+
+def run_and_build_launcher():
+    run_build()
+    run_launcher()
+
 def main():
     parser = argparse.ArgumentParser(description="Manage build and run of elysium project.")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     subparsers.add_parser("build", help="Clean, build frontend and backend")
     subparsers.add_parser("run", help="Run compiled launcher")
+    subparsers.add_parser("runbuild", help="Run compiled launcher")
     subparsers.add_parser("dev", help="Start dev server and run launcher")
     subparsers.add_parser("install", help="Install dependencies")
+    subparsers.add_parser("clean", help="Clean build")
 
     args = parser.parse_args()
 
     if args.command == "build":
-        clean_build()
-        build_app()
-        configure_cmake()
-        build_cmake()
+        run_build()
     elif args.command == "run":
         run_launcher()
+    elif args.command == "runbuild":
+        run_and_build_launcher()
     elif args.command == "dev":
         run_dev()
     elif args.command == "install":
         install_depend()
+    elif args.command == "clean":
+        clean_build()
 
 if __name__ == "__main__":
     main()
